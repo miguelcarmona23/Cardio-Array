334db0ec755cdf0e82ac45ccfbebafbb5fb74d23
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-05 10:02:15 -0500

    Added const comments

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,11 @@ const people = [
     { name: 'Irv', year: 1970 },
     { name: 'Lux', year: 2015 }
 ];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
